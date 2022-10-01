6c344fef881c2562ab185ed69c942cc3bc8f78d4
Hoàng Kim Phú <88869280+KimPhu84@users.noreply.github.com>
2022-10-01 21:17:23 +0700

    Customer Controller
    CRUD Customer

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -38,6 +39,12 @@ namespace BodyBuildingApp
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BodyBuildingApp", Version = "v1" });
             });
+            services.AddDbContext<DBContext>(
+              option =>
+              {
+                  option.UseSqlServer("ConnectionString");
+              }
+              );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
